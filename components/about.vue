cb59ca9d14b1e0680df51b331be5672ec5c43b5f
work-leonid <work.leonid@gmail.com>
2021-10-10 16:33:46 +0300

    init

--- components/about.vue
+++ components/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto py-40">
-    <h1 class="text-3xl text-purple-600">Hi, my name is Leonid Nazarov</h1>
+  <div class="container mx-auto py-20">
+    <h1 class="text-3xl text-purple-600">Hi, my name is Leonid</h1>
   </div>
 </template>
