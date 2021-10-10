abc20dce27734f288bc76c235bbf5677c46e99cf
work-leonid <work.leonid@gmail.com>
2021-10-10 23:15:15 +0300

    feat: add links

--- components/about.vue
+++ components/about.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="container mx-auto py-20">
-    <h1 class="text-3xl text-purple-600">Hi, my name is Leonid</h1>
+  <div class="container mx-auto py-20 px-5">
+    <h1 class="text-3xl mb-5">Hi, my name is Leonid</h1>
+    <p>
+      I'm a designer, now I'm studying programming. I'm learning to do my own projects. I'm learning to write.
+    </p>
     <div class="my-10">
-    <a href="https://twitter.com/nleonid">Twitter</a>
-    <a href="mailto:work.leonid@gmail.com">Mail</a>
+      <a href="https://teletype.in/@leonix" target="_blank" class="block">Blog [ru]</a>
+      <a href="https://twitter.com/nleonid" target="_blank" class="block mb-5">Twitter [ru]</a>
+      <a href="mailto:work.leonid@gmail.com">Mail</a>
     </div>
   </div>
 </template>
