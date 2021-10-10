4900cf09123d7627aaa0ead64824d5306d7b0ee8
work-leonid <work.leonid@gmail.com>
2021-10-10 23:17:57 +0300

    feat: add linkedin link

--- components/about.vue
+++ components/about.vue
@@ -6,7 +6,8 @@
     </p>
     <div class="my-10">
       <a href="https://teletype.in/@leonix" target="_blank" class="block">Blog [ru]</a>
-      <a href="https://twitter.com/nleonid" target="_blank" class="block mb-5">Twitter [ru]</a>
+      <a href="https://twitter.com/nleonid" target="_blank" class="block">Twitter [ru]</a>
+      <a href="https://www.linkedin.com/in/leonid-nazarov-5681917a/" target="_blank" class="block mb-5">Linkedin</a>
       <a href="mailto:work.leonid@gmail.com">Mail</a>
     </div>
   </div>
